71ab755b904a1bdcf9b9c6d19194eadc3f32bb85
LeandroSouzaOliveira <leandro.oliveirasouza1@gmail.com>
2023-08-16 14:53:10 -0300

    Adding links to catalog and product detail pages

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -4,6 +4,7 @@ import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import * as productService from "../../../services/product-service";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function ProductDetails() {
   const params = useParams();
@@ -15,7 +16,9 @@ export default function ProductDetails() {
         {product && <ProductDetailsCard product={product} />}
         <div className="dsc-btn-page-container">
           <ButtonPrimary text="Comprar" />
-          <ButtonInverse text="Início" />
+          <Link to="/">
+            <ButtonInverse text="Início" />
+          </Link>
         </div>
       </section>
     </main>
