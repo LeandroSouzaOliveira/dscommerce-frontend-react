e9d67c73160d0d32cc8e358978a4ef71411f2a75
LeandroSouzaOliveira <leandro.oliveirasouza1@gmail.com>
2023-08-29 17:15:23 -0300

    Requests with Axios for the catalog and by id

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,20 +2,19 @@ import "./styles.css";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-//import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
 import axios from "axios";
 
 export default function ProductDetails() {
-  //const params = useParams();
+  const params = useParams();
   const [product, setProduct] = useState<ProductDTO>();
 
   useEffect(() => {
-    axios.get("http://localhost:8080/products/1")
+    axios.get(`http://localhost:8080/products/${params.productId}`)
       .then(response => {
-        console.log(response.data);
         setProduct(response.data);
       })
   }, []);
