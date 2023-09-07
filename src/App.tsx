ff6a0a377d0077cd9757e5a08d2499f471ce4c6d
LeandroSouzaOliveira <leandro.oliveirasouza1@gmail.com>
2023-09-07 16:30:02 -0300

    Order confirmation page

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { PrivateRoute } from "./components/PrivateRoute";
 import { AccessTokenPayloadDTO } from "./models/auth";
 import * as authService from "./services/auth-service";
 import * as cartService from "./services/cart-service";
+import Confirmation from "./routes/ClientHome/Confirmation";
 
 export default function App() {
   const [contextCartCount, setContextCartCount] = useState<number>(0);
@@ -40,21 +41,12 @@ export default function App() {
             <Route path="/" element={<ClientHome />}>
               <Route index element={<Catalog />} />
               <Route path="catalog" element={<Catalog />} />
-              <Route
-                path="product-details/:productId"
-                element={<ProductDetails />}
-              />
+              <Route path="product-details/:productId" element={<ProductDetails />} />
               <Route path="cart" element={<Cart />} />
               <Route path="login" element={<Login />} />
+              <Route path="confirmation/:orderId" element={<Confirmation />} />
             </Route>
-            <Route
-              path="/admin/"
-              element={
-                <PrivateRoute roles={["ROLE_ADMIN"]}>
-                  <Admin />
-                </PrivateRoute>
-              }
-            >
+            <Route path="/admin/" element={<PrivateRoute roles={['ROLE_ADMIN']}> <Admin /> </PrivateRoute>} >
               <Route index element={<AdminHome />} />
             </Route>
             <Route path="*" element={<Navigate to="/" />} />
