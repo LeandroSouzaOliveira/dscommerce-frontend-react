0b8448c2a2a093231755021268c9c0b9cb3c7e7e
LeandroSouzaOliveira <leandro.oliveirasouza1@gmail.com>
2023-09-09 13:12:32 -0300

    toDirty function of the forms module

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -20,15 +20,18 @@ export function updateAll(inputs: any, newValues: any) {
 }
 
 export function validate(inputs: any, name: string) {
-
   if (!inputs[name].validation) {
     return inputs;
   }
 
   const isInvalid = !inputs[name].validation(inputs[name].value);
-  
+
   return {
     ...inputs,
     [name]: { ...inputs[name], invalid: isInvalid.toString() },
   };
+}
+
+export function toDirty(inputs: any, name: string) {
+  return { ...inputs, [name]: { ...inputs[name], dirty: "true" } };
 }
